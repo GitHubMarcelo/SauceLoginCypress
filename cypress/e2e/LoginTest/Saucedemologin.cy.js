aa2488517aa2b2c74d2aacc0a2ff7581af07e5c3
GitHubMarcelo <dasilvamarcelods@gmail.com>
2024-02-23 16:35:11 +0000

    SauceLoginCypress

---
+++ cypress/e2e/LoginTest/Saucedemologin.cy.js
@@ -0,0 +1,26 @@
+/// <reference types='Cypress'/>
+describe('Login Functinal Test', () => {
+    it('Successfull Login', () => {
+        cy.visit('https://www.saucedemo.com/')
+        cy.get('[data-test="username"]').type('standard_user')
+        cy.get('[data-test="password"]').type('secret_sauce')
+        cy.get('[data-test="login-button"]').click()
+        cy.get('.title').should('contain','Products')
+    });
+
+    it('Unsucessfull Login - Wrong User Name', () => {
+        cy.visit('https://www.saucedemo.com/')
+        cy.get('[data-test="username"]').type('standard_user1')
+        cy.get('[data-test="password"]').type('secret_sauce')
+        cy.get('[data-test="login-button"]').click()
+        cy.get('[data-test="error"]').should('contain','Epic sadface: Username and password do not match any user in this service')
+    });
+
+    it('Unsucessfull Login - Wrong Password', () => {
+        cy.visit('https://www.saucedemo.com/')
+        cy.get('[data-test="username"]').type('standard_user')
+        cy.get('[data-test="password"]').type('secret_sauce2')
+        cy.get('[data-test="login-button"]').click()
+        cy.get('[data-test="error"]').should('contain','Epic sadface: Username and password do not match any user in this service')
+    });
+});
